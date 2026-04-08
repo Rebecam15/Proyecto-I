dce4837ad14ca95afa1e6b9ef13a4b498c6f6190
Rebecam15 <rebecamilara008@gmail.com>
2026-04-08 20:44:38 +0200

    UI

--- Assets/Scripts/SistemaVidas.cs
+++ Assets/Scripts/SistemaVidas.cs
@@ -1,23 +1,31 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UIElements;
 
 public class SistemaVidas : MonoBehaviour
 {
-    [SerializeField] private int vidas;
-    public bool muerto;
+    [SerializeField] private int vidas=3;
+    public bool muerto= false;
+    public UIDocument uiDocument;
+    private Label textoVidas;
 
-    void start()
+    void Start()
     {
-        vidas = 3;
-        muerto = false;
+        textoVidas = uiDocument.rootVisualElement.Q<Label>("textoVidas");//.rootvisualElement da acceso al nivel más alto del contenedor de UI Layout. La Q busca el primer label con el nombre textoVidas.
+       
+        /* Para cambiarlo por corazones o algo así. Cambiar el laber por una imagen.
+         * var image = new Image();
+           image.image = Resources.Load<Texture2D>("sample-image");*/
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision2D collision) //Cuando colisiona
     {
        
-        if (collision.gameObject.CompareTag("Enemigo") && muerto==false)
+        if (collision.gameObject.CompareTag("Enemigo") && muerto==false) //Si está vivo y choca contra un enemigo
         {
             vidas--;
+            textoVidas.text = "Vidas: " + vidas;
             Debug.Log("El jugador ha chocado contra un enemigo");
             if (vidas < 1)
             {
