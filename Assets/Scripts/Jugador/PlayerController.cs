393ccae529e105138a0495127e64139fef5c6cdd
Gena726 <anadq2006@gmail.com>
2026-04-29 17:57:05 +0200

    Limpieza de código

--- Assets/Scripts/Jugador/PlayerController.cs
+++ Assets/Scripts/Jugador/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
 
-    [SerializeField] private float speedX = 8f; // Velocidad al caminar
+    [SerializeField] private float speedX = 8f; 
     private float horizontalInput;
 
     [SerializeField] private float fuerzaSalto = 12f;
@@ -22,15 +22,12 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // OnMove se activa cuando pulsas A/D o Flechas (Configurado en Input Actions)
     public void OnMove(InputValue value)
     {
-        // Solo guardamos la X, porque en plataformas la Y la lleva la gravedad
         Vector2 movementValue = value.Get<Vector2>();
         horizontalInput = movementValue.x;
     }
 
-    // OnJump se activa cuando pulsas Espacio
     public void OnJump(InputValue value)
     {
         if (value.isPressed)
@@ -41,7 +38,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Detectamos si toca el suelo
         enSuelo = Physics2D.Raycast(controlSuelo.position, Vector2.down, distanciaRaycast, capaSuelo);
 
         if (enSuelo)
@@ -52,11 +48,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // MOVIMIENTO HORIZONTAL:
-        // Mantenemos la velocidad actual de Y (gravedad) y cambiamos solo la X
         rb.linearVelocity = new Vector2(horizontalInput * speedX, rb.linearVelocity.y);
 
-        // LÓGICA DE SALTO:
         if (saltoPedido)
         {
             ProcesarSalto();
@@ -79,7 +72,6 @@ public class PlayerController : MonoBehaviour
 
     private void EjecutarSalto()
     {
-        // Reseteamos la velocidad vertical para que el salto siempre sea igual de potente
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
         rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
     }
