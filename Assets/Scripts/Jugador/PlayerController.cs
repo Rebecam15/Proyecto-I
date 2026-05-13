05077888cd5ca66bd8779c6c29b47cb6f0f1c762
Gena726 <anadq2006@gmail.com>
2026-05-13 20:24:06 +0200

    fuerza para pegarse al suelo

--- Assets/Scripts/Jugador/PlayerController.cs
+++ Assets/Scripts/Jugador/PlayerController.cs
@@ -1,3 +1,159 @@
+/*using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+
+
+public class PlayerController : MonoBehaviour
+
+{
+
+    public Rigidbody2D rb;
+
+
+
+    [SerializeField] private float speedX = 8f;
+
+    private float horizontalInput;
+
+
+
+    [SerializeField] private float fuerzaSalto = 12f;
+
+    [SerializeField] private LayerMask capaSuelo;
+
+    [SerializeField] private Transform controlSuelo;
+
+    [SerializeField] private float distanciaRaycast = 0.2f;
+
+
+
+    private bool enSuelo;
+
+    private bool puedeDobleSalto;
+
+    private bool saltoPedido;
+
+
+
+    private void Start()
+
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+
+    public void OnMove(InputValue value)
+
+    {
+
+        Vector2 movementValue = value.Get<Vector2>();
+
+        horizontalInput = movementValue.x;
+
+    }
+
+
+
+    public void OnJump(InputValue value)
+
+    {
+
+        if (value.isPressed)
+
+        {
+
+            saltoPedido = true;
+
+        }
+
+    }
+
+
+
+    void Update()
+
+    {
+
+        enSuelo = Physics2D.Raycast(controlSuelo.position, Vector2.down, distanciaRaycast, capaSuelo);
+
+
+
+        if (enSuelo)
+
+        {
+
+            puedeDobleSalto = true;
+
+        }
+
+    }
+
+
+
+    private void FixedUpdate()
+
+    {
+
+        rb.linearVelocity = new Vector2(horizontalInput * speedX, rb.linearVelocity.y);
+
+
+
+        if (saltoPedido)
+
+        {
+
+            ProcesarSalto();
+
+            saltoPedido = false;
+
+        }
+
+    }
+
+
+
+    private void ProcesarSalto()
+
+    {
+
+        if (enSuelo)
+
+        {
+
+            EjecutarSalto();
+
+        }
+
+        else if (puedeDobleSalto)
+
+        {
+
+            EjecutarSalto();
+
+            puedeDobleSalto = false;
+
+        }
+
+    }
+
+
+
+    private void EjecutarSalto()
+
+    {
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+
+        rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+
+    }
+
+}*/
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,17 +161,20 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
 
-    [SerializeField] private float speedX = 8f; 
+    [SerializeField] private float speedX = 8f;
     private float horizontalInput;
 
     [SerializeField] private float fuerzaSalto = 12f;
     [SerializeField] private LayerMask capaSuelo;
     [SerializeField] private Transform controlSuelo;
     [SerializeField] private float distanciaRaycast = 0.2f;
+    [SerializeField] private float fuerzaPegado = 10f;
 
     private bool enSuelo;
     private bool puedeDobleSalto;
     private bool saltoPedido;
+
+    private bool bloqueadoEnSuelo;
 
     private void Start()
     {
@@ -24,8 +183,7 @@ public class PlayerController : MonoBehaviour
 
     public void OnMove(InputValue value)
     {
-        Vector2 movementValue = value.Get<Vector2>();
-        horizontalInput = movementValue.x;
+        horizontalInput = value.Get<Vector2>().x;
     }
 
     public void OnJump(InputValue value)
@@ -33,6 +191,8 @@ public class PlayerController : MonoBehaviour
         if (value.isPressed)
         {
             saltoPedido = true;
+            
+            bloqueadoEnSuelo = false;
         }
     }
 
@@ -43,12 +203,31 @@ public class PlayerController : MonoBehaviour
         if (enSuelo)
         {
             puedeDobleSalto = true;
+
+            
+            if (!saltoPedido)
+            {
+                bloqueadoEnSuelo = true;
+            }
+        }
+        else
+        {
+            bloqueadoEnSuelo = false;
         }
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontalInput * speedX, rb.linearVelocity.y);
+        float yVel = rb.linearVelocity.y;
+
+      
+        if (bloqueadoEnSuelo && !saltoPedido && rb.linearVelocity.y <= 0.1f)
+        {
+            
+            yVel = -fuerzaPegado;
+        }
+
+        rb.linearVelocity = new Vector2(horizontalInput * speedX, yVel);
 
         if (saltoPedido)
         {
@@ -59,20 +238,17 @@ public class PlayerController : MonoBehaviour
 
     private void ProcesarSalto()
     {
-        if (enSuelo)
+        if (enSuelo || puedeDobleSalto)
         {
-            EjecutarSalto();
-        }
-        else if (puedeDobleSalto)
-        {
-            EjecutarSalto();
-            puedeDobleSalto = false;
-        }
-    }
+            if (!enSuelo)
+            {
+                puedeDobleSalto = false;
+            }
 
-    private void EjecutarSalto()
-    {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
-        rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            bloqueadoEnSuelo = false;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
     }
 }
+
